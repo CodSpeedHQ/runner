8f6b2ea22b32f93b722fae512dc3b12969f14e7e
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-11-24 17:15:26 +0100

    fix(runner): use sudo apt instead of apt

--- src/runner/setup.rs
+++ src/runner/setup.rs
@@ -11,15 +11,15 @@ pub async fn setup(system_info: &SystemInfo) -> Result<()> {
     let valgrind_deb_url = format!("https://github.com/CodSpeedHQ/valgrind-codspeed/releases/download/{}/valgrind_{}_ubuntu-{}_amd64.deb", VALGRIND_CODSPEED_VERSION, VALGRIND_CODSPEED_VERSION, system_info.os_version);
     let deb_path = env::temp_dir().join("valgrind-codspeed.deb");
     download_file(&Url::parse(valgrind_deb_url.as_str()).unwrap(), &deb_path).await?;
-    let update_status = Command::new("apt")
-        .args(["update"])
+    let update_status = Command::new("sudo")
+        .args(["apt", "update"])
         .status()
         .map_err(|_| anyhow!("Failed to update apt"))?;
     if !update_status.success() {
         bail!("Failed to update apt");
     }
-    let install_status = Command::new("apt")
-        .args(["install", "-y", deb_path.to_str().unwrap()])
+    let install_status = Command::new("sudo")
+        .args(["apt", "install", "-y", deb_path.to_str().unwrap()])
         .status()
         .map_err(|_| anyhow!("Failed to install valgrind-codspeed"))?;
     if !install_status.success() {
