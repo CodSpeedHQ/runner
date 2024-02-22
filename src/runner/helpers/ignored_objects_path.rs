85f64ecf0e632f996e8835607f04904a690566ae
Arthur Pastel <arthur.pastel@gmail.com>
2024-02-22 16:28:27 +0100

    feat: handle symlinks in ignored objects

--- src/runner/helpers/ignored_objects_path.rs
+++ src/runner/helpers/ignored_objects_path.rs
@@ -1,5 +1,5 @@
 use crate::prelude::*;
-use std::process::Command;
+use std::{path::PathBuf, process::Command};
 
 fn get_python_objects() -> Vec<String> {
     let output = Command::new("python")
@@ -36,14 +36,26 @@ fn get_node_objects() -> Vec<String> {
         debug!("Failed to get node shared objects: {:?}", output.stderr);
         return vec![];
     }
-    let so_output = String::from_utf8_lossy(&output.stdout).trim().to_string();
-    vec![so_output]
+    let so_str = String::from_utf8_lossy(&output.stdout).trim().to_string();
+    vec![so_str]
+}
+
+fn normalize_objects_path(objects_path_to_ignore: &mut [String]) {
+    for path in objects_path_to_ignore.iter_mut() {
+        let cpath = PathBuf::from(&path).canonicalize();
+        if cpath.is_err() {
+            debug!("Failed to get normalized shared objects: {:?}", cpath.err());
+            continue;
+        }
+        *path = cpath.unwrap().to_string_lossy().to_string();
+    }
 }
 
 pub fn get_objects_path_to_ignore() -> Vec<String> {
     let mut objects_path_to_ignore = vec![];
     objects_path_to_ignore.extend(get_python_objects());
     objects_path_to_ignore.extend(get_node_objects());
+    normalize_objects_path(&mut objects_path_to_ignore);
     debug!("objects_path_to_ignore: {:?}", objects_path_to_ignore);
     objects_path_to_ignore
 }
