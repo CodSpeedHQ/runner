3b0bc04cb26baeec9ec1c6e9635022f0eaae5da4
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2024-07-02 10:03:37 +0000

    feat(runner/poll_results): add regressions threshold, colors and better style to logs

--- src/run/poll_results.rs
+++ src/run/poll_results.rs
@@ -1,10 +1,11 @@
 use std::time::Duration;
 
+use console::style;
 use tokio::time::{sleep, Instant};
 use url::Url;
 
 use crate::api_client::{
-    CodSpeedAPIClient, FetchLocalRunReportRun, FetchLocalRunReportVars, RunStatus,
+    CodSpeedAPIClient, FetchLocalRunReportResponse, FetchLocalRunReportVars, RunStatus,
 };
 use crate::prelude::*;
 
@@ -30,7 +31,7 @@ pub async fn poll_results(
         run_id: run_id.clone(),
     };
 
-    let run;
+    let response;
     loop {
         if start.elapsed() > RUN_PROCESSING_MAX_DURATION {
             bail!("Polling results timed out");
@@ -40,17 +41,18 @@ pub async fn poll_results(
             .fetch_local_run_report(fetch_local_run_report_vars.clone())
             .await?
         {
-            FetchLocalRunReportRun { status, .. } if status != RunStatus::Completed => {
+            FetchLocalRunReportResponse { run, .. } if run.status != RunStatus::Completed => {
                 sleep(Duration::from_secs(5)).await;
             }
-            run_from_api => {
-                run = run_from_api;
+            reponse_from_api => {
+                response = reponse_from_api;
                 break;
             }
         }
     }
 
-    let report = run
+    let report = response
+        .run
         .head_reports
         .into_iter()
         .next()
@@ -58,14 +60,28 @@ pub async fn poll_results(
 
     info!("Report completed, here are the results:");
     if let Some(impact) = report.impact {
-        info!("Impact: {}%", (impact * 100.0).round());
+        let rounded_impact = (impact * 100.0).round();
+        let impact_text = if impact > 0.0 {
+            style(format!("+{}%", rounded_impact)).green().bold()
+        } else {
+            style(format!("{}%", rounded_impact)).red().bold()
+        };
+
+        info!(
+            "Impact: {} (allowed regression: -{}%)",
+            impact_text,
+            (response.allowed_regression * 100.0).round()
+        );
     }
-    info!("Conclusion: {:?}", report.conclusion);
+    info!("Conclusion: {}", report.conclusion);
 
     let mut report_url = Url::parse(config.frontend_url.as_str())?;
-    report_url.set_path(format!("{}/{}/runs/{}", owner, name, run.id).as_str());
+    report_url.set_path(format!("{}/{}/runs/{}", owner, name, response.run.id).as_str());
 
-    info!("\nTo see the full report, visit: {}", report_url);
+    info!(
+        "\nTo see the full report, visit: {}",
+        style(report_url).blue().bold().underlined()
+    );
 
     Ok(())
 }
