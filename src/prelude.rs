7c19915131df2ac556f2da770b8c04c65fc3384f
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2024-07-24 16:22:41 +0000

    chore(clippy): allow some prelude unused imports

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,6 @@
 pub use crate::{end_group, start_group, start_opened_group};
+#[allow(unused_imports)]
 pub use anyhow::{anyhow, bail, ensure, Context, Error, Result};
 pub use itertools::Itertools;
+#[allow(unused_imports)]
 pub use log::{debug, error, info, trace, warn};
