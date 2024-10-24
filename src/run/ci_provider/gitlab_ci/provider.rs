d32be0030d07fda0946f1745acacedbbad639111
fargito <fr.farge96@gmail.com>
2024-10-24 14:33:28 +0200

    feat(gitlab): add detector

---
+++ src/run/ci_provider/gitlab_ci/provider.rs
@@ -0,0 +1,43 @@
+use simplelog::SharedLogger;
+use std::env;
+
+use crate::prelude::*;
+use crate::run::ci_provider::interfaces::ProviderMetadata;
+use crate::run::ci_provider::provider::CIProviderDetector;
+use crate::run::ci_provider::CIProvider;
+use crate::run::config::Config;
+
+#[derive(Debug)]
+pub struct GitLabCIProvider {}
+
+impl TryFrom<&Config> for GitLabCIProvider {
+    type Error = Error;
+    fn try_from(_config: &Config) -> Result<Self> {
+        Ok(Self {})
+    }
+}
+
+impl CIProviderDetector for GitLabCIProvider {
+    fn detect() -> bool {
+        // check if the GITLAB_CI environment variable is set and the value is truthy
+        env::var("GITLAB_CI") == Ok("true".into())
+    }
+}
+
+impl CIProvider for GitLabCIProvider {
+    fn get_logger(&self) -> Box<dyn SharedLogger> {
+        unimplemented!()
+    }
+
+    fn get_provider_name(&self) -> &'static str {
+        "GitLab CI"
+    }
+
+    fn get_provider_slug(&self) -> &'static str {
+        "gitlab-ci"
+    }
+
+    fn get_provider_metadata(&self) -> Result<ProviderMetadata> {
+        unimplemented!()
+    }
+}
