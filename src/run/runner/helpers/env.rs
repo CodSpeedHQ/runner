48d1afbbcd0f0171b23cbc8434159b4e1a1fefeb
Arthur Pastel <arthur@codspeed.io>
2024-10-30 14:46:19 +0100

    feat: only pass `PYTHONMALLOC` to the valgrind instrument (#48)

--- src/run/runner/helpers/env.rs
+++ src/run/runner/helpers/env.rs
@@ -5,7 +5,6 @@ use lazy_static::lazy_static;
 lazy_static! {
     pub static ref BASE_INJECTED_ENV: HashMap<&'static str, String> = {
         HashMap::from([
-            ("PYTHONMALLOC", "malloc".into()),
             ("PYTHONHASHSEED", "0".into()),
             ("ARCH", ARCH.into()),
             ("CODSPEED_ENV", "runner".into()),
