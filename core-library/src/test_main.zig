e3db0f7038e92dd39733d23c53062faa5e588fe3
not-matthias <matthias@codspeed.io>
2025-04-15 17:55:56 +0200

    chore: fmt code

--- core-library/src/test_main.zig
+++ core-library/src/test_main.zig
@@ -9,12 +9,12 @@ pub fn main() !void {
         if (result) |_| {
             try std.fmt.format(out, "[SUCCESS] {s}\n", .{name});
         } else |err| {
-            try std.fmt.format(out, "[FAIL] {s}: {}\n", .{t.name, err});
+            try std.fmt.format(out, "[FAIL] {s}: {}\n", .{ t.name, err });
         }
     }
 }
 
 fn extractName(t: std.builtin.TestFn) []const u8 {
     const marker = std.mem.lastIndexOf(u8, t.name, ".test.") orelse return t.name;
-    return t.name[marker+6..];
+    return t.name[marker + 6 ..];
 }
