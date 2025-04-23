28a3e9f724ddb0b9a2e880829805992b08d33559
not-matthias <matthias@codspeed.io>
2025-04-23 18:10:41 +0200

    feat: pin tokio worker threads to core 0

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod run;
 mod setup;
 
 use console::style;
+use core_affinity::CoreId;
 use lazy_static::lazy_static;
 use local_logger::clean_logger;
 use prelude::*;
@@ -27,8 +28,28 @@ lazy_static! {
         format!("{VALGRIND_CODSPEED_BASE_VERSION}-0codspeed1");
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
+fn main() -> anyhow::Result<()> {
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(4)
+        .on_thread_start(|| {
+            // Core 0 is also responsible for interrupts and other system tasks. But
+            // this is fine, as we're not executing performance-critical code.
+            if !core_affinity::set_for_current(CoreId { id: 0 }) {
+                panic!("failed to pin worker thread to core 0");
+            }
+        })
+        .enable_all()
+        .build()?;
+
+    // NOTE: The code above doesn't set the core affinity for the main thread. We should avoid doing so, as
+    // the affinity is passed down to child threads/processes.
+
+    let _guard = runtime.enter();
+    runtime.block_on(real_main());
+    Ok(())
+}
+
+async fn real_main() {
     let res = crate::app::run().await;
     if let Err(err) = res {
         for cause in err.chain() {
