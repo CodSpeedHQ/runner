cfbbd65c94062bed8c4b6e51c579fc927a2bdd72
not-matthias <matthias@codspeed.io>
2025-06-26 18:46:50 +0200

    fix: run perf with sudo; support systemd-run for non-perf walltime

--- src/run/runner/wall_time/executor.rs
+++ src/run/runner/wall_time/executor.rs
@@ -1,11 +1,12 @@
 use super::perf::PerfRunner;
 use crate::prelude::*;
+use crate::run::RunnerMode;
 use crate::run::instruments::mongo_tracer::MongoTracer;
 use crate::run::runner::executor::Executor;
 use crate::run::runner::helpers::env::get_base_injected_env;
 use crate::run::runner::helpers::get_bench_command::get_bench_command;
 use crate::run::runner::helpers::run_command_with_log_pipe::run_command_with_log_pipe;
-use crate::run::runner::{ExecutorName, RunData, RunnerMode};
+use crate::run::runner::{ExecutorName, RunData};
 use crate::run::{check_system::SystemInfo, config::Config};
 use async_trait::async_trait;
 use std::fs::canonicalize;
@@ -20,6 +21,20 @@ impl WallTimeExecutor {
         Self {
             perf: cfg!(target_os = "linux").then(PerfRunner::new),
         }
+    }
+
+    fn walltime_bench_cmd(config: &Config, run_data: &RunData) -> Result<String> {
+        let bench_cmd = get_bench_command(config)?;
+
+        let setenv = get_base_injected_env(RunnerMode::Walltime, &run_data.profile_folder)
+            .into_iter()
+            .map(|(env, value)| format!("--setenv={env}={value}"))
+            .join(" ");
+        let uid = nix::unistd::Uid::current().as_raw();
+        let gid = nix::unistd::Gid::current().as_raw();
+        Ok(format!(
+            "systemd-run --scope --slice=codspeed.slice --same-dir --uid={uid} --gid={gid} {setenv} -- {bench_cmd}"
+        ))
     }
 }
 
@@ -44,29 +59,24 @@ impl Executor for WallTimeExecutor {
         run_data: &RunData,
         _mongo_tracer: &Option<MongoTracer>,
     ) -> Result<()> {
-        // IMPORTANT: Don't use `sh` here! We will use this pid to send signals to the
-        // spawned child process which won't work if we use a different shell.
-        let mut cmd = Command::new("bash");
-
-        cmd.envs(get_base_injected_env(
-            RunnerMode::Walltime,
-            &run_data.profile_folder,
-        ));
+        let mut cmd = Command::new("sudo");
 
         if let Some(cwd) = &config.working_directory {
             let abs_cwd = canonicalize(cwd)?;
             cmd.current_dir(abs_cwd);
         }
 
-        let bench_cmd = get_bench_command(config)?;
-        let status = match (config.enable_perf, &self.perf) {
-            (true, Some(perf)) => perf.run(cmd, &bench_cmd, config).await,
-            _ => {
-                cmd.args(["-c", &bench_cmd]);
-                debug!("cmd: {:?}", cmd);
+        let bench_cmd = Self::walltime_bench_cmd(config, run_data)?;
 
-                run_command_with_log_pipe(cmd).await
-            }
+        let status = if let Some(perf) = &self.perf
+            && config.enable_perf
+        {
+            perf.run(cmd, &bench_cmd, config).await
+        } else {
+            cmd.args(["sh", "-c", &bench_cmd]);
+            debug!("cmd: {cmd:?}");
+
+            run_command_with_log_pipe(cmd).await
         };
 
         let status =
@@ -82,13 +92,15 @@ impl Executor for WallTimeExecutor {
 
     async fn teardown(
         &self,
-        _config: &Config,
+        config: &Config,
         _system_info: &SystemInfo,
         run_data: &RunData,
     ) -> Result<()> {
         debug!("Copying files to the profile folder");
 
-        if let Some(perf) = &self.perf {
+        if let Some(perf) = &self.perf
+            && config.enable_perf
+        {
             perf.save_files_to(&run_data.profile_folder).await?;
         }
 
