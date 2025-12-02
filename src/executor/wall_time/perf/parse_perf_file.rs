f39e236754bdd8c2f71b9491005cd36a46fec81e
Guillaume Lagrange <guillaume@glagrange.eu>
2025-12-02 02:20:39 +0100

    feat: parse perf file for memmap events instead of relying on /proc/pid/maps

---
+++ src/executor/wall_time/perf/parse_perf_file.rs
@@ -0,0 +1,121 @@
+use super::perf_map::ProcessSymbols;
+use super::unwind_data::UnwindDataExt;
+use libc::pid_t;
+use linux_perf_data::PerfFileReader;
+use linux_perf_data::PerfFileRecord;
+use linux_perf_data::linux_perf_event_reader::EventRecord;
+use runner_shared::unwind_data::UnwindData;
+use std::collections::HashMap;
+use std::path::Path;
+
+use crate::prelude::*;
+
+pub struct MemmapRecordsOutput {
+    pub symbols_by_pid: HashMap<pid_t, ProcessSymbols>,
+    pub unwind_data_by_pid: HashMap<pid_t, Vec<UnwindData>>,
+}
+
+pub(super) fn parse_for_memmap2(perf_file_path: &Path) -> Result<MemmapRecordsOutput> {
+    let reader = std::fs::File::open(perf_file_path).unwrap();
+    let mut symbols_by_pid = HashMap::<pid_t, ProcessSymbols>::new();
+    let mut unwind_data_by_pid = HashMap::<pid_t, Vec<UnwindData>>::new();
+
+    let PerfFileReader {
+        mut perf_file,
+        mut record_iter,
+    } = PerfFileReader::parse_pipe(reader)?;
+
+    const PROT_EXEC: u32 = 0x4;
+
+    while let Some(record) = record_iter.next_record(&mut perf_file).unwrap() {
+        let PerfFileRecord::EventRecord { record, .. } = record else {
+            continue;
+        };
+
+        let Ok(parsed_record) = record.parse() else {
+            continue;
+        };
+
+        let EventRecord::Mmap2(record) = parsed_record else {
+            continue;
+        };
+
+        let record_path_string = {
+            let path_slice = record.path.as_slice();
+            String::from_utf8_lossy(&path_slice).into_owned()
+        };
+
+        let end_addr = record.address + record.length;
+
+        if record_path_string == "//anon" {
+            // Skip anonymous mappings
+            trace!(
+                "Skipping anonymous mapping: {:x}-{:x}",
+                record.address, end_addr
+            );
+            continue;
+        }
+
+        if record_path_string.starts_with("[") && record_path_string.ends_with("]") {
+            // Skip special mappings
+            debug!(
+                "Skipping special mapping: {} - {:x}-{:x}",
+                record_path_string, record.address, end_addr
+            );
+            continue;
+        }
+
+        debug!(
+            "Pid {}: {:016x}-{:016x} {:08x} {:?} (Prot {:?})",
+            record.pid,
+            record.address,
+            end_addr,
+            record.page_offset,
+            record_path_string,
+            record.protection,
+        );
+
+        if record.protection & PROT_EXEC == 0 {
+            continue;
+        }
+
+        symbols_by_pid
+            .entry(record.pid)
+            .or_insert(ProcessSymbols::new(record.pid))
+            .add_mapping(
+                record.pid,
+                &record_path_string,
+                record.address,
+                end_addr,
+                record.page_offset,
+            );
+        debug!("Added symbols mapping for module {record_path_string:?}");
+
+        match UnwindData::new(
+            record_path_string.as_bytes(),
+            record.page_offset,
+            record.address,
+            end_addr,
+            None,
+        ) {
+            Ok(unwind_data) => {
+                unwind_data_by_pid
+                    .entry(record.pid)
+                    .or_default()
+                    .push(unwind_data);
+                debug!(
+                    "Added unwind data for {record_path_string} ({:x} - {:x})",
+                    record.address, end_addr
+                );
+            }
+            Err(error) => {
+                debug!("Failed to create unwind data for module {record_path_string}: {error}");
+            }
+        }
+    }
+
+    Ok(MemmapRecordsOutput {
+        symbols_by_pid,
+        unwind_data_by_pid,
+    })
+}
